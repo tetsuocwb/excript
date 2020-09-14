926a2e1d19f65bc106af176cf474beceafaf81d3
Claudio Antunes <tetsuo@MacBook-Air-de-Claudio.local>
2020-09-13 22:14:15 -0300

    search name

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("oi")
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
+import xlrd
+import difflib
+book = xlrd.open_workbook("/Users/tetsuo/dev/python/Relacaodegalerias.xls")
+gal = book.sheet_by_name("GAL-CUB")
+nome = gal.col_values(4)
+aprox = []
+for a in nome:
+    aprox.append(str(a))
+
+print(nome[2])
+#print(aprox)
+P1 = process.extract("DIEGO ", aprox, limit=20)
+print(P1)
+print(difflib.get_close_matches("DIEGO", aprox, 3, 0.2))
